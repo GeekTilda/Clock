a29eec71b752b4f19ed6cf56b327f164aa35f2c3
Tilda Lindblad Aderud <lindbladaderud.tilda@elev.ga.ntig.se>
2023-02-14 14:51:20 +0100

    We added dots and numbers, also positioned the clock in the middle :)

--- src/ClockFaze.java
+++ src/ClockFaze.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class ClockFaze {
+import javax.swing.*;
+import java.awt.*;
+
+public class ClockFaze extends JPanel { //Yes, I can apperently not gammar.
+    private int size;
+    private int middle;
+    final double angle = (2*Math.PI)/12;
+    private int cirkSize;
+    private Dimension dim = new Dimension(size,size);
+
+    public ClockFaze(int size) {
+        this.size = size;
+        middle = size/2;
+        this.setBackground(Color.pink); //Sets the background color
+        this.setPreferredSize(new Dimension(size,size));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        face(g);
+        drawNum(g);
+    }
+
+    public void face(Graphics g) {
+        int x = (int) (size*0.1);
+        int y = x;
+        cirkSize = size - x*2;
+        g.setColor(Color.gray);
+        g.fillOval(x,y,cirkSize,cirkSize);
+    }
+
+    public void drawNum(Graphics g) {
+        int dx,dy;
+        int dist = (int) (cirkSize*0.9)/2;
+        for (int i = 1; i <= 12; i++) {
+            double v = angle*i;
+            dx = (int) (dist * Math.sin(v)) + middle;
+            dy = (int) (-dist * Math.cos(v)) + middle;
+            g.setColor(Color.BLACK);
+            g.drawString(String.valueOf(i),dx,dy);
+            g.fillOval(dx,dy,7,7);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        start();
+    }
+    public static void start() {
+        int size = 250;
+        //Basic frame setup
+        JFrame myFrame = new JFrame("The clock :D "); //Starts the frame which everything will happen on.
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets the frame to exit when closed.
+        myFrame.setSize(new Dimension(size, size)); //Size of the frame
+        myFrame.setResizable(false);
+        //Sets the clockface (clockfaze) to the frame
+        myFrame.add(new ClockFaze(size));
+        //Packages the frame
+        myFrame.pack();
+        myFrame.setVisible(true);
     }
 }
